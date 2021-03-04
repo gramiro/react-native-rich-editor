f92018ca5b2bcc32b77edf3f077c548bf44b023c
Ramiro Guerrero <ramiro.g92@gmail.com>
2021-03-04 23:56:33 +0100

    fix: blocks toggles

--- src/editor.js
+++ src/editor.js
@@ -242,13 +242,13 @@ function createHTML(options = {}) {
             italic: { state: function() { return queryCommandState('italic'); }, result: function() { return exec('italic'); }},
             underline: { state: function() { return queryCommandState('underline'); }, result: function() { return exec('underline'); }},
             strikeThrough: { state: function() { return queryCommandState('strikeThrough'); }, result: function() { return exec('strikeThrough'); }},
-            heading1: { state: function() { return queryCommandValue(formatBlock) === 'h1'; }, result: function() { return exec(formatBlock, '<h1>'); }},
-            heading2: { state: function() { return queryCommandValue(formatBlock) === 'h2'; }, result: function() { return exec(formatBlock, '<h2>'); }},
-            heading3: { state: function() { return queryCommandValue(formatBlock) === 'h3'; }, result: function() { return exec(formatBlock, '<h3>'); }},
-            heading4: { state: function() { return queryCommandValue(formatBlock) === 'h4'; }, result: function() { return exec(formatBlock, '<h4>'); }},
-            heading5: { state: function() { return queryCommandValue(formatBlock) === 'h5'; }, result: function() { return exec(formatBlock, '<h5>'); }},
-            heading6: { state: function() { return queryCommandValue(formatBlock) === 'h6'; }, result: function() { return exec(formatBlock, '<h6>'); }},
-            paragraph: { state: function() { return queryCommandValue(formatBlock) === 'p'; }, result: function() { return exec(formatBlock, '<p>'); }},
+            heading1: { state: function() { return queryCommandValue(formatBlock) === 'h1'; }, result: function() { return queryCommandValue(formatBlock) === 'h1' ? exec(formatBlock, 'div') : exec(formatBlock, '<h1>'); }},
+            heading2: { state: function() { return queryCommandValue(formatBlock) === 'h2'; }, result: function() { return queryCommandValue(formatBlock) === 'h2' ? exec(formatBlock, 'div') : exec(formatBlock, '<h2>'); }},
+            heading3: { state: function() { return queryCommandValue(formatBlock) === 'h3'; }, result: function() { return queryCommandValue(formatBlock) === 'h3' ? exec(formatBlock, 'div') : exec(formatBlock, '<h3>'); }},
+            heading4: { state: function() { return queryCommandValue(formatBlock) === 'h4'; }, result: function() { return queryCommandValue(formatBlock) === 'h4' ? exec(formatBlock, 'div') : exec(formatBlock, '<h4>'); }},
+            heading5: { state: function() { return queryCommandValue(formatBlock) === 'h5'; }, result: function() { return queryCommandValue(formatBlock) === 'h5' ? exec(formatBlock, 'div') : exec(formatBlock, '<h5>'); }},
+            heading6: { state: function() { return queryCommandValue(formatBlock) === 'h6'; }, result: function() { return queryCommandValue(formatBlock) === 'h6' ? exec(formatBlock, 'div') : exec(formatBlock, '<h6>'); }},
+            paragraph: { state: function() { return queryCommandValue(formatBlock) === 'p'; }, result: function() { return queryCommandValue(formatBlock) === 'p' ? exec(formatBlock, 'div') : exec(formatBlock, '<p>'); }},
             quote: { result: function() { return exec(formatBlock, '<blockquote>'); }},
             removeFormat: { result: function() { return exec('removeFormat'); }},
             orderedList: {
@@ -562,4 +562,4 @@ function createHTML(options = {}) {
 }
 
 const HTML = createHTML();
-export {HTML, createHTML, getContentCSS};
+export { HTML, createHTML, getContentCSS };
